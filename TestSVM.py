ce05de12afe43c42f43cb5be1441a70f1369cba6
Lior Birman <birmanlior@gmail.com>
2020-09-12 19:43:38 +0300

    main instructions changes

--- TestSVM.py
+++ TestSVM.py
@@ -1,9 +1,8 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import datasets
 from SupportVectorMachine import LinearSVM
-from SupportVectorMachine import f1_score
 import Plotting as p
+from main import *
+
 
 # dataGenerator():
 # input:
@@ -19,28 +18,22 @@ import Plotting as p
 
 
 def dataGenerator(n_samples, cluster_std, n_features=2, centers=2, random_state=40):
-
     # create data for training & predictions
-    X, y = datasets.make_blobs(n_samples=n_samples, n_features=n_features, centers=centers,
-                               cluster_std=cluster_std, random_state=random_state)
+    X_data, y_data = datasets.make_blobs(n_samples=n_samples, n_features=n_features, centers=centers,
+                                         cluster_std=cluster_std, random_state=random_state)
 
-    return X, y
+    return X_data, y_data
 
 
 # create SVM object
-alpha_param = 0.0001
-lambda_param = 0.01
-n_iterations = 15000
 svm_object = LinearSVM(alpha_param=alpha_param, lambda_param=lambda_param, n_iterations=n_iterations)
 
 # data generation
-samples = 500
-clusterStd = 2
-whole_data, whole_labels = dataGenerator(samples, clusterStd)
+whole_data, whole_labels = dataGenerator(training_cv_samples, training_cv_clusterStd)
 
 # 70% for training, 30% for cross validation
-training_portion = int(samples * 0.7)
-prediction_portion = int(samples * 0.3)
+training_portion = int(training_cv_samples * 0.7)
+prediction_portion = int(training_cv_samples * 0.3)
 
 # training data
 X = whole_data[0:training_portion, :]
@@ -58,17 +51,17 @@ y_predicted = svm_object.predict(X_cv)
 
 # show on screen
 figure_title = "Training & Classification"
-p.plotSVM(X, X_cv, y, y_cv, w, b, figure_title, y_predicted, iterations, lambda_param, alpha_param, samples, training_portion, prediction_portion)
-
+p.plotSVM(X, X_cv, y, y_cv, w, b, figure_title, y_predicted, iterations, lambda_param, alpha_param, training_cv_samples,
+          training_portion, prediction_portion)
 
 # new prediction data generation
-samples = 166
-clusterStd = 5
-X_test, y_test = dataGenerator(samples, clusterStd)
+X_test, y_test = dataGenerator(test_samples, test_clusterStd)
+prediction_portion = test_samples
 
 # predict classifications of cross validation data
 y_predicted = svm_object.predict(X_test)
 
 # show on screen
 figure_title = "Original Training & Noisy Data Classification"
-p.plotSVM(X, X_test, y, y_test, w, b, figure_title, y_predicted, iterations, lambda_param, alpha_param, samples, training_portion, prediction_portion)
+p.plotSVM(X, X_test, y, y_test, w, b, figure_title, y_predicted, iterations, lambda_param, alpha_param, test_samples,
+          training_portion, prediction_portion)
--- main.py
+++ main.py
@@ -12,17 +12,24 @@
 #    Results:
 #               - Noisy data will be generated.
 #               - Noisy data prediction will occur.
-#               - The training and data results will be shown on a window.
+#               - The original training and the new data results will be shown on a window.
 
-# 3. Close the window to proceed to the next test.
-#    Results:
-#               - Bigger noise data will be generated.
-#               - Bigger noise data prediction will occur.
-#               - The training and data results will be shown on a window.
+# 3.Change parameters in "main.py" to affect "TestSVM.py run"
+#   alpha_param = learning rate.
+#   lambda_param = regularization.
+#   n_iterations = maximum iterations of training.
+#   training_cv_samples = amount of data samples for training & cross validation (70%, 30%).
+#   training_cv_clusterSTD = standard deviation of the training & cross validation data samples.
+#   test_samples = amount of data samples for classification test.
+#   test_clusterSTD = standard deviation of the test data samples.
 
-import matplotlib.pyplot as plt
-plt.figure()
-x = 0.9876543
-plt.text(0.018, 0.493, "{:.3f}".format(x))
+alpha_param = 0.0001
+lambda_param = 0.01
+n_iterations = 15000
 
-plt.show()
+training_cv_samples = 500
+training_cv_clusterStd = 2
+
+test_samples = 150
+test_clusterStd = 5
+
